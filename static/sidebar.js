6707c0ecd61afdeb9841842c7075b128afb81c8d
Envying <jon_Sun20@hotmail.com>
2018-09-21 11:45:48 +1000

    file name change

---
+++ static/sidebar.js
@@ -0,0 +1,25 @@
+function openWorld(evt, worldName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("world");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+  }
+  document.getElementById(worldName).style.display = "block";
+  evt.currentTarget.className += " w3-red";
+}
+
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+      x.previousElementSibling.className += " w3-green";
+  } else {
+      x.className = x.className.replace(" w3-show", "");
+      x.previousElementSibling.className =
+      x.previousElementSibling.className.replace(" w3-green", "");
+  }
+}
