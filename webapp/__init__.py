6b87deb11413c33eaa2a5d34e4c30f6ce46d2fbb
Envying <jon_Sun20@hotmail.com>
2018-10-10 21:54:20 +1100

    cloudsql changes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,9 @@ import logging
 
 from flask import current_app, Flask, redirect, request, session, url_for
 import httplib2
+
 from oauth2client.contrib.flask_util import UserOAuth2
+
 
 oauth2 = UserOAuth2()
 
