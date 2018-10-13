ebfa5f1b0e4f4e931f82855ab31d2d83a21597b3
Envying <jon_Sun20@hotmail.com>
2018-10-13 22:18:37 +1100

    added new packages

--- webapp/routes.py
+++ webapp/routes.py
@@ -4,9 +4,9 @@ import os
 from flask import render_template, url_for, flash, redirect, request, abort
 from webapp import app
 # db, bcrypt, mail
-# from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
+# from webapp.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
 #                              PostForm, RequestResetForm, ResetPasswordForm)
-# from flaskblog.models import User, Post
+# from webapp.models import User, Post
 # from flask_login import login_user, current_user, logout_user, login_required
 # from flask_mail import Message
 
