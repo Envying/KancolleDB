81a13936901c2216984f82ec17237f677420a50e
Envying <jon_Sun20@hotmail.com>
2018-10-14 21:14:13 +1100

    added oauth

---
+++ webapp/oauth.py
@@ -0,0 +1,16 @@
+from webapp import auth_config
+from requests_oauthlib import OAuth2Session
+
+def get_google_auth(state=None, token=None):
+    if token:
+        return OAuth2Session(Auth.CLIENT_ID, token=token)
+    if state:
+        return OAuth2Session(
+            Auth.CLIENT_ID,
+            state=state,
+            redirect_uri=Auth.REDIRECT_URI)
+    oauth = OAuth2Session(
+        Auth.CLIENT_ID,
+        redirect_uri=Auth.REDIRECT_URI,
+        scope=Auth.SCOPE)
+    return oauth
