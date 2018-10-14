982432c91368778d7d6b522453a8dca9f9938ba4
Envying <jon_Sun20@hotmail.com>
2018-10-14 23:13:31 +1100

    added db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -6,7 +6,7 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kancolledbpw@127.0.0.1:3306/kancolleDB'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kancolledbpw@127.0.0.1:3306/admin'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
