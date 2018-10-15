9de4624cbcc54f96759c15cde66eb66ba072f602
Envying <jon_Sun20@hotmail.com>
2018-10-15 15:39:41 +1100

    comments and new GAE

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from werkzeug.serving import make_ssl_devcert
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -11,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kancolledbpw@127.0.0.1:330
 db = SQLAlchemy(app)
 
 # use this to run the SSL to run locally on 127.0.0.1:8080
+from werkzeug.serving import make_ssl_devcert
 make_ssl_devcert('./ssl', host='localhost')
 
 login_manager = LoginManager(app)
