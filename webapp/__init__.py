e4ff9f225f120b2daeaa354b84d2f461750f5f94
Envying <jon_Sun20@hotmail.com>
2018-10-15 11:17:26 +1100

    commenting

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:kancolledbpw@127.0.0.1:3306/admin'
 db = SQLAlchemy(app)
+
+# use this to run the SSL to run locally on 127.0.0.1:8080
 make_ssl_devcert('./ssl', host='localhost')
+
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = 'login'
