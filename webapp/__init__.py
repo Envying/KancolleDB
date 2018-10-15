2c1113d763bf5dcff301578b10e2cbb8ab62cf22
Envying <jon_Sun20@hotmail.com>
2018-10-15 18:21:30 +1100

    init changes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
-from webapp.cloudSQL import gen_connection
+from webapp.cloudsql import gen_connection
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
--- webapp/cloudsql.py
+++ webapp/cloudsql.py
@@ -1,3 +1,5 @@
+import os
+
 class SQL:
     SQL_CONNECTION_NAME = 'kancolledb-project:australia-southeast1:kancolle-mysql'
     SQL_USER = 'root'
@@ -8,7 +10,7 @@ def gen_connection():
     # if not on Google then use local MySQL
     if not os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
         conn_template = 'mysql://%s:%s@127.0.0.1:3306/%s'
-        return conn_template % (SQl.SQL_USER, SQL.SQL_PASSWORD, SQL.SQL_INSTANCE) 
+        return conn_template % (SQL.SQL_USER, SQL.SQL_PASSWORD, SQL.SQL_INSTANCE)
     else:
         conn_template = 'mysql+mysqldb://%s:%s@/%s?unix_socket=/cloudsql/%s'
-        return conn_template % (SQl.SQL_USER, SQL.SQL_PASSWORD, SQL.SQL_INSTANCE, SQL.SQL_CONNECTION_NAME)
+        return conn_template % (SQL.SQL_USER, SQL.SQL_PASSWORD, SQL.SQL_INSTANCE, SQL.SQL_CONNECTION_NAME)
