522a6dee58c05815e132eddc7f9513445b428139
Envying <jon_Sun20@hotmail.com>
2018-10-16 21:43:15 +1100

    main.py added os.env

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import os
 from webapp import app
 
 # Run without the SSL (GAE)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
 
 # Run with a SSL (Locally)
 # if __name__ == '__main__':
