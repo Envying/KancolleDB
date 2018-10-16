41c0145310a2b7ea667241ee8da05de698d7f6e0
Envying <jon_Sun20@hotmail.com>
2018-10-16 19:03:57 +1100

    added application id to app.yaml

--- webapp/oauth.py
+++ webapp/oauth.py
@@ -5,9 +5,9 @@ class Auth:
     CLIENT_ID = ('236022772111-r1ls58b9ps2vs6q7t1b4g2hr2nqf4jdm.apps.googleusercontent.com')
     CLIENT_SECRET = 'gCfyefl41AhZzZK-8ok53efG'
 # SLL host
-    REDIRECT_URI = 'https://127.0.0.1:8080/oAuthcallback'
+    # REDIRECT_URI = 'https://127.0.0.1:8080/oAuthcallback'
 # local host
-    # REDIRECT_URI = 'http://localhost:8080/oAuthcallback'
+    REDIRECT_URI = 'http://localhost:8080/oAuthcallback'
 # Google App Engine
     # REDIRECT_URI = 'https://kancolledb-project.appspot.com/oAuthcallback'
     AUTH_URI = 'https://accounts.google.com/o/oauth2/auth'
